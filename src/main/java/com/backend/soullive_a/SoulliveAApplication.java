a093e934890f1f82943d41ac124f533624afb296
j2noo <wlsdndml213@gmail.com>
2024-02-22 01:14:43 +0900

    init : 스프링 스프링 프로젝트 초기 세팅

---
+++ src/main/java/com/backend/soullive_a/SoulliveAApplication.java
@@ -0,0 +1,13 @@
+package com.backend.soullive_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoulliveAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoulliveAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/soullive_a/SoulliveAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.soullive_a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoulliveAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
