add6eb3bf8a9b499160fa99e893192d690be5092
j2noo <wlsdndml213@gmail.com>
2024-02-22 02:15:13 +0900

    docker test

---
+++ src/main/java/com/backend/soullive_a/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.backend.soullive_a.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/api")
+    public String apiTest() {
+        System.out.println("@@@");
+        return "test@!!##@#@!";
+    }
+}
