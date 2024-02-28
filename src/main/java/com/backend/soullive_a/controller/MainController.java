9fefab94200cb7ad34fe1767006f726ba9ee8f6a
Jinwoo Park <wlsdndml213@gmail.com>
2024-02-29 04:31:26 +0900

    [Chore] ci/cd 재설정 (#2)
    * feat : health-check api 추가
    
    * feat : cicd ë¡스크립트 수정

--- src/main/java/com/backend/soullive_a/controller/MainController.java
+++ src/main/java/com/backend/soullive_a/controller/MainController.java
@@ -6,9 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
 
-    @GetMapping("/api")
+    @GetMapping("/health")
     public String apiTest() {
-        System.out.println("@@@");
-        return "감사합니다.이것이 cicd이다 쏘울라이브 버전";
+        System.out.println("health-check!");
+        return "health check!";
     }
 }
+
