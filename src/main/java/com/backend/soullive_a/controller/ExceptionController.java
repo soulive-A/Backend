c52a741c89c16ee2be49d42b08668ba5f8d86241
leesangmin <dkwkghkdlxld2@gmail.com>
2024-03-02 19:38:00 +0900

    feat: @AuthenticationPrincipal 예시 추가

--- src/main/java/com/backend/soullive_a/controller/ExceptionController.java
+++ src/main/java/com/backend/soullive_a/controller/ExceptionController.java
@@ -1,11 +1,15 @@
 package com.backend.soullive_a.controller;
 
+import com.backend.soullive_a.entity.User;
 import com.backend.soullive_a.exception.base.BaseResponse;
 import com.backend.soullive_a.exception.custom.NotFoundUserException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class ExceptionController {
     @GetMapping("/success")
     public BaseResponse<Object> getUser() {
@@ -20,5 +24,13 @@ public class ExceptionController {
     @GetMapping("/fail")
     public BaseResponse<Object> notFoundUser() {
         throw new NotFoundUserException();
+    }
+    /*
+     * @AuthenticationPrincipal 사용법 예시
+     */
+    @GetMapping("/test")
+    public String test(@AuthenticationPrincipal User user){
+        log.info("user 아이디 값: {}",user.getId());
+        return "test";
     }
 }
