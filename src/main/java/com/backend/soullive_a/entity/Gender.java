892810e595cf39232eebe80313b337718f4bef6f
Somin Yun <somin455@naver.com>
2024-03-03 23:00:48 +0900

    feat: #10 product entity 수정

--- src/main/java/com/backend/soullive_a/entity/Gender.java
+++ src/main/java/com/backend/soullive_a/entity/Gender.java
@@ -6,8 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.hibernate.annotations.DynamicInsert;
 
 @Entity
 @Table(name = "GENDER")
--- src/main/java/com/backend/soullive_a/entity/Product.java
+++ src/main/java/com/backend/soullive_a/entity/Product.java
@@ -5,11 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.hibernate.annotations.DynamicInsert;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Table(name = "PRODUCT")
