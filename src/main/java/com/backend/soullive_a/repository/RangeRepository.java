199bb607573501020571c64a0bf57179a1854dbc
leesangmin <dkwkghkdlxld2@gmail.com>
2024-03-05 00:17:54 +0900

    feat: #19 광고 상품 삭제 api 테스트 코드 작성

--- src/main/java/com/backend/soullive_a/repository/RangeRepository.java
+++ src/main/java/com/backend/soullive_a/repository/RangeRepository.java
@@ -1,6 +1,7 @@
 package com.backend.soullive_a.repository;
 
 import com.backend.soullive_a.entity.Range;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,5 @@ import java.util.List;
 @Repository
 public interface RangeRepository extends JpaRepository<Range, Long> {
     List<Range> findAllByProductId(Long productId);
+    Optional<Range> findByProductId(Long productId);
 }
