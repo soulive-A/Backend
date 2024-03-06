6f69ab7cb5dac2b5b7e325a18cd02712b3e47f35
leesangmin <dkwkghkdlxld2@gmail.com>
2024-03-07 04:04:22 +0900

    docs: swagger 명세서 설명 추가

--- src/main/java/com/backend/soullive_a/controller/ModelController.java
+++ src/main/java/com/backend/soullive_a/controller/ModelController.java
@@ -5,11 +5,14 @@ import com.backend.soullive_a.dto.response.ModelResponse;
 import com.backend.soullive_a.dto.response.RecentModelResponse;
 import com.backend.soullive_a.exception.base.BaseResponse;
 import com.backend.soullive_a.service.ModelService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
+@Tag(name = "모델 API", description = "모델 조회,생성,최근 조회한 모델 API입니다.")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/model")
@@ -21,6 +24,7 @@ public class ModelController {
      * @param modelName
      * @return
      */
+    @Operation(summary = "모델 조회 API", description = "모델 기본 정보를 조회하는 API입니다.")
     @GetMapping("")
     public BaseResponse<ModelResponse> getModel(@RequestParam String modelName, @RequestParam Long productId) {
         return BaseResponse.<ModelResponse>builder()
@@ -31,12 +35,12 @@ public class ModelController {
                 .build();
 
     }
-
     /**
      * 모델 등록 api
      * @param request
      * @return
      */
+    @Operation(summary = "모델 생성 API", description = "모델을 생성하는 API입니다.")
     @PostMapping("")
     public BaseResponse<ModelResponse> createModel(@RequestBody @Valid ModelRequest request) {
         System.out.println("@");
@@ -47,5 +51,16 @@ public class ModelController {
                 .data(modelService.createModel(request))
                 .build();
 
+    }
+    @Operation(summary = "최근 조회 모델 API", description = "최근 조회한 모델들을 조회하는 API입니다.")
+    @GetMapping("/recent/{productId}")
+    public BaseResponse<List<RecentModelResponse>> getRecentModel(@Parameter(name = "상품 id",description = "상품 기본키(필수)",
+        required = true) @PathVariable Long productId){
+        return BaseResponse.<List<RecentModelResponse>>builder()
+            .isSuccess(true)
+            .code(200)
+            .message("최근 조회한 모델 조회 성공")
+            .data(modelService.getRecentModel(productId))
+            .build();
     }
 }
--- src/main/java/com/backend/soullive_a/controller/ModelIntroductionController.java
+++ src/main/java/com/backend/soullive_a/controller/ModelIntroductionController.java
@@ -1,15 +1,15 @@
 
 package com.backend.soullive_a.controller;
 
-import com.backend.soullive_a.dto.request.ModelIntroduceRequest;
+
 import com.backend.soullive_a.dto.response.ModelIntroductionResponse;
-import com.backend.soullive_a.dto.response.ModelResponse;
 import com.backend.soullive_a.exception.base.BaseResponse;
 import com.backend.soullive_a.service.ModelIntroductionService;
-import jakarta.validation.Valid;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
+@Tag(name = "모델 소개 API", description = "모델 소개 API입니다.")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/model/introduction")
@@ -22,6 +22,7 @@ public class ModelIntroductionController {
      * @param modelName
      * @return
      */
+    @Operation(summary = "모델 소개 API", description = "모델 소개 정보를 조회하는 API입니다.")
     @GetMapping("")
     public BaseResponse<ModelIntroductionResponse> getModelIntroduction(@RequestParam String modelName) {
         return BaseResponse.<ModelIntroductionResponse>builder()
