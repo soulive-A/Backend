5fd0caab29883763a93accaa179fcf21ec3c7d9e
leesangmin <dkwkghkdlxld2@gmail.com>
2024-03-07 04:26:28 +0900

    fix: ci build시 발생하는 SQLIntegrityConstraintViolationException 해결

--- src/main/java/com/backend/soullive_a/controller/ModelController.java
+++ src/main/java/com/backend/soullive_a/controller/ModelController.java
@@ -2,6 +2,7 @@ package com.backend.soullive_a.controller;
 
 import com.backend.soullive_a.dto.request.ModelRequest;
 import com.backend.soullive_a.dto.response.ModelResponse;
+import com.backend.soullive_a.dto.response.RecentModelResponse;
 import com.backend.soullive_a.exception.base.BaseResponse;
 import com.backend.soullive_a.service.ModelService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -35,23 +36,6 @@ public class ModelController {
 
     }
 
-    /**
-     * 모델 등록 api
-     * @param request
-     * @return
-     */
-    @Operation(summary = "모델 생성 API", description = "모델을 생성하는 API입니다.")
-    @PostMapping("")
-    public BaseResponse<ModelResponse> createModel(@RequestBody @Valid ModelRequest request) {
-        System.out.println("@");
-        return BaseResponse.<ModelResponse>builder()
-                .isSuccess(true)
-                .code(200)
-                .message("모델 생성에 성공했습니다.")
-                .data(modelService.createModel(request))
-                .build();
-
-    }
     @Operation(summary = "최근 조회 모델 API", description = "최근 조회한 모델들을 조회하는 API입니다.")
     @GetMapping("/recent/{productId}")
     public BaseResponse<List<RecentModelResponse>> getRecentModel(@Parameter(name = "상품 id",description = "상품 기본키(필수)",
--- src/main/java/com/backend/soullive_a/service/ModelService.java
+++ src/main/java/com/backend/soullive_a/service/ModelService.java
@@ -1,15 +1,11 @@
 package com.backend.soullive_a.service;
 
-import com.backend.soullive_a.dto.request.ModelRequest;
 import com.backend.soullive_a.dto.response.ModelResponse;
 import com.backend.soullive_a.dto.response.RecentModelResponse;
 import java.util.List;
 
 public interface ModelService {
     public ModelResponse getModel(String modelName, Long productId);
-
-
-    public ModelResponse createModel(ModelRequest request);
 
     public List<RecentModelResponse> getRecentModel(Long productId);
 }
