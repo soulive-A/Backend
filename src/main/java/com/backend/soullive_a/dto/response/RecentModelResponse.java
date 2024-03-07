2533b4bb67cb8320b67dc642fd9674f7ea2c1577
leesangmin <dkwkghkdlxld2@gmail.com>
2024-03-07 12:36:07 +0900

    docs: swagger 문서 추가

--- src/main/java/com/backend/soullive_a/dto/response/RecentModelResponse.java
+++ src/main/java/com/backend/soullive_a/dto/response/RecentModelResponse.java
@@ -4,13 +4,13 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "최근 조회한 모델 model 객체")
 public record RecentModelResponse(
-    @Schema(name = "모델 id(기본키)")
+    @Schema(name = "모델 id(기본키)", example = "1")
     Long modelId,
-    @Schema(name = "모델 이미지 url")
+    @Schema(name = "모델 이미지 url", example = "https://soullive-bucket.s3.ap-northeast-2.amazonaws.com/heeae_profile.png")
     String imageUrl,
-    @Schema(name = "모델 이름")
+    @Schema(name = "모델 이름", example = "한지민")
     String modelName,
-    @Schema(name = "ai 추천 점수")
+    @Schema(name = "ai 추천 점수", example = "3.0")
     Float aiRate
 ) {
 
