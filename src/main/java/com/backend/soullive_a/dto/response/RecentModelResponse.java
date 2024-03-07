9c2e2e1de69e74e7c06a9155a4a7dac4499d095e
leesangmin <dkwkghkdlxld2@gmail.com>
2024-03-07 12:17:09 +0900

    docs: swagger 문서 추가

--- src/main/java/com/backend/soullive_a/dto/response/RecentModelResponse.java
+++ src/main/java/com/backend/soullive_a/dto/response/RecentModelResponse.java
@@ -1,9 +1,16 @@
 package com.backend.soullive_a.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "최근 조회한 모델 model 객체")
 public record RecentModelResponse(
+    @Schema(name = "모델 id(기본키)")
     Long modelId,
+    @Schema(name = "모델 이미지 url")
     String imageUrl,
+    @Schema(name = "모델 이름")
     String modelName,
+    @Schema(name = "ai 추천 점수")
     Float aiRate
 ) {
 
